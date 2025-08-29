380b4702a749369037b1c10453c1dbab656bb3b8
magabeni <magabenista@gmail.com>
2025-08-29 10:08:13 -0300

    Adicionado getters e setters

--- src/ClasseAutomotores.java
+++ src/ClasseAutomotores.java
@@ -1,4 +1,47 @@
 
 public class ClasseAutomotores {
 
+		String corAt;
+		String marcaAt;
+		String modeloAt;
+		String tipoAt;
+		
+		public ClasseAutomotores() {
+			
+		}
+		
+		public ClasseAutomotores(String corPar, String marcaPar, String modeloPar, String tipoPar) {
+			this.corAt = corPar;
+			this.marcaAt = marcaPar;
+			this.modeloAt = modeloPar;
+			this.tipoAt = tipoPar;
+		}
+		//Getters
+		public String pegarCor() {
+			return corAt;
+		}
+		public String pegarMarca() {
+			return marcaAt;
+		}
+		public String pegarModelo() {
+			return modeloAt;
+		}
+		public String pegarTipo() {
+			return tipoAt;
+		}
+		//Setters
+		public void alterarCor (String corPar) {
+			this.corAt = corPar;
+		}
+		public void alterarMarca (String marcaPar) {
+			this.marcaAt = marcaPar;
+		}
+		public void alterarModelo (String modeloPar) {
+			this.modeloAt = modeloPar;
+		}
+		public void alterarTipo (String tipoPar) {
+			this.tipoAt = tipoPar;
+		}
 }
+		
+
